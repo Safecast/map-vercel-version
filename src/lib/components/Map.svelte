6681f1cf9ca2410eca97a40f9d57cfdae9d877c9
Stephan Thiel <stephan@nand.io>
2024-03-04 22:49:42 +0100

    initial base map and skeleton

---
+++ src/lib/components/Map.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	import { Map, MapStyle, config } from '@maptiler/sdk';
+	import '@maptiler/sdk/dist/maptiler-sdk.css';
+
+	let map;
+	let mapContainer;
+
+	config.apiKey = 'eALiQdzsgc1xP3bhMxyo';
+
+	onMount(() => {
+		const initialState = { lng: 141.021, lat: 37.424, zoom: 12 };
+
+		map = new Map({
+			container: mapContainer,
+			style:
+				'https://api.maptiler.com/maps/b946056e-74c5-4c2b-bfeb-4c580c588f62/style.json?key=eALiQdzsgc1xP3bhMxyo',
+			center: [initialState.lng, initialState.lat],
+			zoom: initialState.zoom
+		});
+	});
+
+	onDestroy(() => {
+		if (map) {
+			map.remove();
+		}
+	});
+</script>
+
+<div class="h-screen" bind:this={mapContainer} />
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { page } from '$app/stores';
+	import { AddressBookOutline } from 'flowbite-svelte-icons';
+	import { sineIn } from 'svelte/easing';
+	import {
+		DarkMode,
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Sidebar,
+		SidebarGroup,
+		SidebarItem,
+		SidebarWrapper,
+		Drawer,
+		CloseButton,
+		SidebarDropdownWrapper
+	} from 'flowbite-svelte';
+
+	let transitionParams = {
+		x: -320,
+		duration: 200,
+		easing: sineIn
+	};
+
+	let width: number;
+	let drawerHidden: boolean = false;
+	let backdrop: boolean = false;
+	let activateClickOutside = true;
+
+	const toggleDrawer = () => {
+		console.log('toggleDrawer');
+		drawerHidden = !drawerHidden;
+	};
+
+	$: activeUrl = $page.url.pathname;
+</script>
+
+<svelte:window bind:innerWidth={width} />
+<header class="flex-none w-full mx-auto bg-white dark:bg-slate-950">
+	<Navbar fluid>
+		<NavHamburger
+			class="md:block focus:outline-none hover:bg-gray-100 dark:hover:bg-gray-600"
+			onClick={toggleDrawer}
+		/>
+		<NavBrand href="/" class="">
+			<img src="/logo.svg" class="me-3 h-6 sm:h-9" alt="Safecast Logo" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+				>SAFECAST</span
+			>
+		</NavBrand>
+	</Navbar>
+</header>
+
+<Drawer
+	transitionType="fly"
+	{transitionParams}
+	bind:hidden={drawerHidden}
+	bind:activateClickOutside
+	width="w-64"
+	class="overflow-scroll pb-32"
+	id="sidebar"
+>
+	<div class="flex items-center">
+		<CloseButton on:click={() => (drawerHidden = true)} class="mb-4 dark:text-white" />
+	</div>
+	<Sidebar asideClass="w-54">
+		<SidebarWrapper divClass="overflow-y-auto py-2 px-1 dark:bg-gray-800">
+			<SidebarGroup>
+				<SidebarItem label="About this map" href="/about" active={activeUrl === `/about`} />
+				<SidebarItem label="Donate" href="https://blog.safecast.org/donate/" />
+				<SidebarItem label="News" href="https://blog.safecast.org/" />
+			</SidebarGroup>
+		</SidebarWrapper>
+	</Sidebar>
+</Drawer>
+
+<div class="flex mx-auto w-full">
+	<main class="w-full mx-auto h-full">
+		<slot />
+	</main>
+</div>
