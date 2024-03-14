101fe932cef4435b21afb2c8a116996de36ca36d
Stephan Thiel <stephan@nand.io>
2024-03-14 23:15:53 +0100

    load devices

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,23 +1,47 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
-	import { Map, MapStyle, config } from '@maptiler/sdk';
+	import { Map, Marker, config } from '@maptiler/sdk';
+	import type { MapOptions } from '@maptiler/sdk';
 	import '@maptiler/sdk/dist/maptiler-sdk.css';
+	import type { Device } from '$lib/types';
 
-	let map;
-	let mapContainer;
+	export let devices: Device[] = [];
+
+	let map: Map | null = null;
+	let mapContainer: HTMLElement;
 
 	config.apiKey = 'eALiQdzsgc1xP3bhMxyo';
 
 	onMount(() => {
 		const initialState = { lng: 141.021, lat: 37.424, zoom: 12 };
-
-		map = new Map({
+		const options: MapOptions = {
 			container: mapContainer,
 			style:
 				'https://api.maptiler.com/maps/b946056e-74c5-4c2b-bfeb-4c580c588f62/style.json?key=eALiQdzsgc1xP3bhMxyo',
 			center: [initialState.lng, initialState.lat],
 			zoom: initialState.zoom
-		});
+		};
+		map = new Map(options);
+
+		console.log(devices);
+		for (const device of devices) {
+			if (device.loc_lon && device.loc_lat) {
+				var el = document.createElement('div');
+				el.className = 'marker';
+				el.style.backgroundImage = `url(markers/safecast.png)`;
+				el.style.width = '25px';
+				el.style.height = '25px';
+
+				// el.addEventListener('click', function () {
+				// 	window.alert(marker.properties.message);
+				// });
+				const marker = new Marker({
+					element: el
+				})
+					.setLngLat([device.loc_lon, device.loc_lat])
+					.addTo(map);
+			}
+		}
 	});
 
 	onDestroy(() => {
---
+++ src/lib/types.ts
@@ -0,0 +1,147 @@
+export enum DeviceClass {
+  AIRNOTE_SOLAR_AIR = "product:org.airnote.solar.air.v1",
+  AIRNOTE_SOLAR_RAD = "product:org.airnote.solar.rad.v1",
+  AIRNOTE_SOLAR = "product:org.airnote.solar.v1",
+  BLUES_RADNOTE = "product:com.blues.radnote",
+  BLUES_AIRNOTE = "product:com.blues.airnote",
+  OZZIE_RADNOTE = "product:net.ozzie.ray:radnote",
+  SAFECAST = "safecast",
+  POINTCAST = "pointcast",
+  GEIGIECAST = "geigiecast",
+  NGEIGIE = "ngeigie",
+  KITTYWOOD = "product:biz.yr-design.rob:kittywood",
+  GNOTE = "product:com.blues.gnote",
+  NODE01 = "product:org.softwarelivre.luisfelipe:node01"
+}
+
+export enum Orientation {
+  FACE_UP = "face-up",
+  FACE_DOWN = "face-down",
+  LANDSCAPE_LEFT = "landscape-left",
+  LANDSCAPE_RIGHT = "landscape-right",
+  PORTRAIT_DOWN = "portrait-down",
+  PORTRAIT_UP = "portrait-up",
+  ANGLED = "angled",
+}
+
+export interface Device {
+  device_urn: string;
+  loc_name: string;
+  loc_country: string;
+  device_sn: string | null;
+  device_contact_name: string | null;
+  device_contact_email: string | null;
+  device: number;
+  when_captured: Date; // "2021-08-25T00:00:00Z"
+  env_temp: number;
+  env_humid: number
+  env_press: number;
+  bat_voltage: number;
+  lnd_7318c: number;
+  pms_pm02_5: number;
+  pms_aqi: number;
+  dev_temp: number;
+  dev_orientation: Orientation;
+  dev_dashboard: string;
+}
+
+export interface DeviceFull {
+  device_urn: string;
+  device_class: DeviceClass;
+  device: number;
+  device_sn: string | null;
+  device_contact_name: string | null;
+  device_contact_email: string | null;
+  device_contact_org: string;
+  device_contact_role: string;
+
+  when_captured: Date; // "2021-08-25T00:00:00Z"
+
+  loc_lat: number;
+  loc_lon: number;
+  loc_name: string;
+  loc_country: string;
+  loc_zone: string;
+
+  lnd_712u: number;
+  lnd_7128ec: number;
+  lnd_7318c: number;
+  lnd_7318u: number;
+  lnd_78017w: number;
+
+  service_uploaded: Date; // "2021-08-25T00:00:00Z"
+  service_transport: string;
+
+  dev_dashboard: string;
+  dev_temp: number;
+  dev_rat: string;
+  dev_bars: number;
+  dev_moved: Date; // "2021-08-25T00:00:00Z"
+  dev_orientation: Orientation;
+  dev_motion: boolean | null;
+  dev_uptime: number;
+  dev_transmitted_bytes: number;
+  dev_received_bytes: number;
+  dev_motion_events: number;
+  dev_oneshots: number;
+  dev_oneshot_seconds: number;
+  dev_humid: number;
+  dev_press: number;
+  dev_comms_resets: number;
+  dev_comms_power_fails: number
+  dev_comms_ant_fails: number
+  dev_overcurrent_events: number
+  dev_test: boolean | null;
+  dev_comms_failures: number;
+  dev_restarts: number;
+  dev_free_memory: number;
+  dev_ntp_count: number;
+  dev_last_failure: string;
+
+  env_temp: number;
+  env_humid: number
+  env_press: number;
+
+  bat_voltage: number;
+  bat_current: number;
+  bat_charge: number;
+
+  gateway_received: Date; // "2021-08-25T00:00:00Z"
+  gateway_lora_snr: number;
+
+  pms_c00_30: number;
+  pms_c00_50: number;
+  pms_c01_00: number;
+  pms_c02_50: number;
+  pms_c05_00: number;
+  pms_c10_00: number;
+  pms_csecs: number;
+  pms_std01_0: number;
+  pms_std02_5: number;
+  pms_std10_0: number;
+  pms_pm01_0: number;
+  pms_pm02_5: number;
+  pms_pm10_0: number;
+
+  pms2_c00_30: number;
+  pms2_c00_50: number;
+  pms2_c01_00: number;
+  pms2_c02_50: number;
+  pms2_c05_00: number;
+  pms2_c10_00: number;
+  pms2_csecs: number;
+  pms2_pm01_0: number;
+  pms2_pm02_5: number;
+  pms2_pm10_0: number;
+
+  opc_c00_38: number;
+  opc_c00_54: number;
+  opc_c01_00: number;
+  opc_c02_10: number;
+  opc_c05_00: number;
+  opc_c10_00: number;
+  opc_csecs: number;
+  opc_pm01_0: number;
+  opc_pm02_5: number;
+  opc_pm10_0: number;
+}
