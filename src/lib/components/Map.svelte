111e019700dcc5d5cb9bbf04b319e6344ebccc84
Rob Oudendijk <rob@yr-design.biz>
2024-03-24 13:22:22 +0900

    Update Map.svelte
    dark map

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -61,7 +61,7 @@
 		const options: MapOptions = {
 			container: mapContainer,
 			style:
-				'https://api.maptiler.com/maps/b946056e-74c5-4c2b-bfeb-4c580c588f62/style.json?key=eALiQdzsgc1xP3bhMxyo',
+				'https://api.maptiler.com/maps/basic-v2-dark/?key=eALiQdzsgc1xP3bhMxyo',
 			center: [initialState.lng, initialState.lat],
 			zoom: initialState.zoom
 		};
